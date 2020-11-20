844a8aa5807edb15d210e85ef674ec78680454ca
sarahlcox <sarahl.cox.22@gmail.com>
2020-11-19 18:15:38 -0600

    updated class comp

--- client/src/components/EmployeeDirectory.js
+++ client/src/components/EmployeeDirectory.js
@@ -1,25 +1,39 @@
-import React, {useEffect, useState} from "react";
+import React, {Component, useEffect, useState} from "react";
 import API from "../utils/API.js";
+import SearchBar from "./SearchBar";
 import Table from "./Table.js";
-import empData from "../utils/empData.json"
-// change to class component 
-const EmployeeDirectory = () => {
+
+// change to class component
+class EmployeeDirectory extends Component {
     // define use state
-    const [userState, setUserState] = useState(empData);
-    // useEffect(() => {
-    //     API.searchName().then(user => {
-    //       setUserState(user);
-    //         console.log(user);
-    //     //   set user state
-    //       //add in reposnse here that renders to page name, email, phone, cell, thumbnail
-    //     })
-    // }, [userState]);
+    state = {
+        user: []
+    }
+    componentDidMount () {
+        console.log("hi")
+        API.searchName().then(response => {
+            console.log(response);
+          this.setState({ user : response.data.results });
+            console.log(this.state.user);
+        //   set user state
+          //add in reposnse here that renders to page name, email, phone, cell, thumbnail
+        })
+    }
+
+render (){
+
+    
 return (
     <div>
-        <Table data={userState}/>
+        <Table data={this.state.user}/>
+        {/* map //pass down into table row.. props drilling taking too </div> */}
+        <TableRow />
         {/* pass down state as a prop */}
     </div>
-)
+)}
 };
+
+
+
 
 export default EmployeeDirectory;
